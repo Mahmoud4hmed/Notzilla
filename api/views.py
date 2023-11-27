07e3454a8d8f639e6f69acc85b52b984eddbcba3
Mahmoud Ahmed <mahmoudahmed9go@gmail.com>
2023-11-27 10:12:02 +0200

    Clearign tests

--- api/views.py
+++ api/views.py
@@ -7,42 +7,6 @@ from .serializers import NoteSerializer
 from rest_framework import status
 
 @api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        {
-            'Endpoint': '/notes/',
-            'method': 'GET',
-            'body': None,
-            'description': 'Returns an array of notes'
-        },
-        {
-            'Endpoint': '/notes/id',
-            'method': 'GET',
-            'body': None,
-            'description': 'Returns a single note object'
-        },
-        {
-            'Endpoint': '/notes/create/',
-            'method': 'POST',
-            'body': {'body': ""},
-            'description': 'Creates new note with data sent in post request'
-        },
-        {
-            'Endpoint': '/notes/id/update/',
-            'method': 'PUT',
-            'body': {'body': ""},
-            'description': 'Creates an existing note with data sent in post request'
-        },
-        {
-            'Endpoint': '/notes/id/delete/',
-            'method': 'DELETE',
-            'body': None,
-            'description': 'Deletes and exiting note'
-        },
-    ]
-    return Response(routes)
-
-@api_view(['GET'])
 def getNotes(request):
     Notes = Note.objects.all().order_by('-updated')
     serializer = NoteSerializer(Notes, many=True)
